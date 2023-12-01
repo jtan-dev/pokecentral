f91f772e48fc6e55f90c1f7348b1a560f17ded91
xopherv <cjayvergel@gmail.com>
2023-12-01 09:52:28 -0600

    Validations re-added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,12 @@
 class Product < ApplicationRecord
+  has_many :product_orders
+  has_many :orders, through: :product_orders
   belongs_to :category
+
+  validates :name, :price, :description, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :name, uniqueness: true
+  validates :stock, numericality: { greater_than_or_equal_to: 0 }
 
   has_one_attached :image
 end
