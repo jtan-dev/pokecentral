80f19346712bab0e66a8c1a29940b3a101074bbd
jtan-dev <joseph.tan096@gmail.com>
2023-12-11 15:08:24 -0600

    modified views to allow partials

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,29 @@
 class CategoriesController < ApplicationController
+  before_action :initialize_session
+  before_action :load_cart
+
   def show
     @category = Category.find(params[:id])
     @products = @category.products.page(params[:page]).per(15)
   end
+
+  def initialize_session
+    session[:cart] ||= []
+end
+
+def add_to_cart
+    id = params[:id].to_i
+    session[:cart] << id unless session[:cart].include?(id)
+    redirect_to root_path
+end
+
+def remove_from_cart
+    id = params[:id].to_i
+    session[:cart].delete(id)
+    redirect_to root_path
+end
+
+def load_cart
+    @cart = Product.find(session[:cart])
+end
 end
