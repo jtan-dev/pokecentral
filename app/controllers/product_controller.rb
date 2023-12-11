3a7ea37da11b8935d584d681e9216539c85c8fb8
xopherv <cjayvergel@gmail.com>
2023-12-11 07:21:25 -0600

    Added cart functionality with buttons and cart list

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,10 +1,32 @@
 class ProductController < ApplicationController
+    before_action :initialize_session
+    before_action :load_cart
     def index
         @products = Product.page(params[:page]).per(15)
     end
 
     def show
         @product = Product.find(params[:id])
+    end
+
+    def initialize_session
+        session[:cart] ||= []
+    end
+
+    def add_to_cart
+        id = params[:id].to_i
+        session[:cart] << id unless session[:cart].include?(id)
+        redirect_to root_path
+    end
+
+    def remove_from_cart
+        id = params[:id].to_i
+        session[:cart].delete(id)
+        redirect_to root_path
+    end
+    
+    def load_cart
+        @cart = Product.find(session[:cart])
     end
 
     def search
