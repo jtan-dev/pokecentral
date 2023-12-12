e1be55817c9308c99b45748b40d532aa8a3eb038
jtan-dev <joseph.tan096@gmail.com>
2023-12-11 21:49:00 -0600

    added account sign up only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :load_categories, :load_pages
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -9,5 +10,11 @@ class ApplicationController < ActionController::Base
 
   def load_pages
     @pages = ContactAboutPage.all
+  end
+
+  def configure_permitted_parameters
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:province_id])
+    end
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,17 @@
 class Customer < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :orders
   belongs_to :province
-  
-  validates :username, :email, presence: true
-  validates :postal_code, length: { is: 6 }
+
+  validates :email, presence: true
+  validate :postal_code_length
+
+  private
+
+  def postal_code_length
+    errors.add(:postal_code, 'must have either 0 or 6 characters') unless [0, 6].include?(postal_code.to_s.length)
+  end
 end
