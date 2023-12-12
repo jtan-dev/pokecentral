806f559d13849c4474965d11e5daa06f4e154f47
jtan-dev <joseph.tan096@gmail.com>
2023-12-11 22:04:31 -0600

    Completed User Accounts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:province_id])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:street_address, :city, :postal_code, :province_id])
     end
   end
 end
